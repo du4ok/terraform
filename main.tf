8a067b0a47367d1be33fa40e8f8bb55ace4bc390
andrii.yaremenko <andrii.yaremenko@intellecteu.com>
2021-12-12 20:26:46 +0200

    Added input and output variables

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+variable "server_port" {
+  description = "Port that server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of created server"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-00d5e377dd7fad751"
   instance_type = "t2.micro"
@@ -9,7 +20,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   tags = {
     Name = "terraform-example"
@@ -19,8 +30,8 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
